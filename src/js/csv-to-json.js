9da94aeee2eb6d1c56369596b2195c8e1cc0659c
Christopher Parker <christopher.c.parker@gmail.com>
2013-07-13 13:29:56 -0400

    Issue #3 / Pull Request #4: Finish refactoring CSV parser as a standalone library. Update unit tests written by @truher to use new parser API.

---
+++ src/js/csv-to-json.js
@@ -0,0 +1,63 @@
+/**
+ * csv-to-json: A utility that converts data format from CSV to JSON.
+ * Copyright (C) 2009-2013 Christopher Parker <http://www.cparker15.com/>
+ *
+ * csv-to-json is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * csv-to-json is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with csv-to-json.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function () {
+    function setMessage (message, error) {
+        var messageEl        = document.getElementById('message'),
+            errorClass       = 'error',
+            messageContainer = document.createElement('p');
+
+        while (messageEl.lastChild) {
+            messageEl.removeChild(messageEl.lastChild);
+        }
+
+        messageContainer.appendChild(document.createTextNode(message));
+        messageEl.appendChild(messageContainer);
+
+        if (error) {
+            messageEl.className += ' ' + errorClass;
+        } else {
+            messageEl.className = messageEl.className.replace(new RegExp(' ' + errorClass, 'g'), '');
+        }
+    }
+
+    window.onload = function () {
+        document.getElementById('convertForm').onsubmit = function () {
+            var csvField  = this.elements['csv'],
+                jsonField = this.elements['json'],
+                csvText   = csvField.value,
+                csvObjects, jsonText;
+
+            try {
+                csvObjects = CSV.parse(csvText);
+
+                jsonText = JSON.stringify(csvObjects, null, '\t');
+
+                jsonField.value = jsonText;
+
+                setMessage('CSV successfully converted to JSON.');
+            } catch (e) {
+                var message = 'Could not convert CSV to JSON: ' + e.message;
+
+                setMessage(message, true);
+            }
+
+            return false;
+        };
+    };
+}());
--- test/tests.js
+++ test/tests.js
@@ -1,65 +1,34 @@
-test("csv2json without header", function() {
-    var input = "asdf";
+test('parse without header', function () {
+    var input = 'asdf';
+
     throws(
-        function() { csvToJson(input); },
+        function () { CSV.parse(input); },
         /missing header/,
-        "should say 'missing header'"
+        'should say "missing header"'
     );
 });
 
-test("csv2json empty", function() {
-    var input = "";
+test('parse empty', function () {
+    var input = '';
+
     throws(
-        function() { csvToJson(input); },
+        function () { CSV.parse(input); },
         /empty input/,
-        "should say 'empty input'"
+        'should say "empty input"'
     );
 });
 
-test("csv2json", function() {
-    var input = "foo,bar\na,b";
-    var actual = csvToJson(input);
-    var expected =
-    '[\n'+
-    '\t{\n'+
-    '\t\t"foo": "a",\n'+
-    '\t\t"bar": "b"\n'+
-    '\t}\n'+
-    ']'
-    equal(actual, expected)
-});
+test('parse', function () {
+    var input    = 'foo,bar\na,b',
 
-test("trim whitespace", function() {
-    equal(trimWhitespace("as df", "as df"));
-    equal(trimWhitespace("asdf", "asdf"));
-    equal(trimWhitespace(" asdf ", "asdf"));
-    equal(trimWhitespace("\tasdf ", "asdf"));
-    equal(trimWhitespace("\t asdf ", "asdf"));
-    equal(trimWhitespace(" as df ", "as df"));
-});
+        expected = '[\n' +
+                       '\t{\n' +
+                           '\t\t"foo": "a",\n' +
+                           '\t\t"bar": "b"\n' +
+                       '\t}\n' +
+                   ']',
 
-test("trim quotes", function() {
-    equal(trimWhitespace("\"asdf\"", "asdf"));
-    equal(trimWhitespace("\'asdf\'", "asdf"));
-    // ?
-    equal(trimWhitespace("\''asdf\'", "asdf"));
-    equal(trimWhitespace("\''''''asdf\'", "asdf"));
-});
+        actual   = JSON.stringify(CSV.parse(input), null, '\t');
 
-test("remove empty rows", function() {
-    deepEqual(removeEmptyRows(['a','b']), ['a','b']);
-    deepEqual(removeEmptyRows(['a','','b']), ['a','b']);
-});
-
-test("parse csv line", function() {
-    deepEqual(parseCSVLine("a,b"), ['a','b']);
-});
-
-test("parse rows", function() {
-    deepEqual(parseRows(['a,b','c,d']),[['a','b'],['c','d']])
-});
-
-test("convert to json", function() {
-    deepEqual(convertToJson([['a','b'],['c','d']]), [{a:'c', b:'d'}])
-    deepEqual(convertToJson([['a','b'],['c','d'],['e','f']]), [{a:'c', b:'d'},{a:'e',b:'f'}])
+    equal(actual, expected);
 });
